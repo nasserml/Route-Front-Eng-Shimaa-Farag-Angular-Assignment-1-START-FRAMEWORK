efa90c18160d9eb45aff63a62d42800cda4d463f
Mahmoud Nasser <mnasserone@gmail.com>
2024-07-29 17:34:14 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Assignment-1-START-FRAMEWORK-Angular';
+}
